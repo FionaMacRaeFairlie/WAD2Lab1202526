797c0d804ce3ea2af17b47b5fc78ab1b183a259b
Fiona Fairlie <f.fairlie@gcu.ac.uk>
2025-11-26 17:36:53 +0000

    update imaage name

--- example2.js
+++ example2.js
@@ -1,32 +1,45 @@
-var http = require('http'),fs = require('fs');
-function serveStaticFile(res, path, contentType, responseCode) {
-    if(!responseCode) responseCode = 200;
-    fs.readFile(__dirname + path, function(err,data) {
-        if(err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('500 - Internal Error');
-        } else {
-            res.writeHead(responseCode,
-                { 'Content-Type': contentType });
-            res.end(data);
-        }
-    });
-}
-http.createServer(function(req,res){
-    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-    switch(path) {
-        case '':
-            serveStaticFile(res, '/public/home.html', 'text/html');
-            break;
-        case '/about':
-            serveStaticFile(res, '/public/about.html', 'text/html');
-            break;
-        case '/img/coffee.jpg':
-            serveStaticFile(res, '/public/img/coffee.jpg','image/jpg');
-            break;
-        default:
-            serveStaticFile(res, '/public/404.html', 'text/html',404);
-            break;
+import http from "http";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// __dirname replacement for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function serveStaticFile(res, filePath, contentType, responseCode = 200) {
+  fs.readFile(path.join(__dirname, filePath), (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("500 - Internal Error");
+    } else {
+      res.writeHead(responseCode, { "Content-Type": contentType });
+      res.end(data);
     }
-}).listen(3000);
-console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
+  });
+}
+
+const server = http.createServer((req, res) => {
+  let requestPath = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+
+  switch (requestPath) {
+    case "":
+      serveStaticFile(res, "/public/home.html", "text/html");
+      break;
+
+    case "/about":
+      serveStaticFile(res, "/public/about.html", "text/html");
+      break;
+
+    case "/img/coffee.jpg":
+      serveStaticFile(res, "/public/img/logo.jpg", "image/jpeg");
+      break;
+
+    default:
+      serveStaticFile(res, "/public/404.html", "text/html", 404);
+      break;
+  }
+});
+
+server.listen(3000);
+console.log("Server started on localhost:3000; press Ctrl-C to terminate....");
--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-var http = require('http');
-http.createServer(function(req,res){
-    // normalize url by removing querystring, optional
-    // trailing slash, and making it lowercase
-    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-    switch(path) {
-        case '':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('Homepage');
-            break;
-        case '/about':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('About');
-         break;
-        default:
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('Not Found');
-        break;
-    }
-}).listen(3000);
-console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  // normalize url by removing querystring, optional trailing slash, and making it lowercase
+  let path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+
+  switch (path) {
+    case "":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Homepage");
+      break;
+
+    case "/about":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("About");
+      break;
+
+    default:
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Not Found");
+      break;
+  }
+});
+
+server.listen(3000);
+
+console.log("Server started on localhost:3000; press Ctrl-C to terminate....");
+
+// http
+//   .createServer(function (req, res) {
+//     res.writeHead(200, { "Content-type": "text/plain" });
+//     res.end("Hello from my Node.js application!");
+//   })
+//   .listen(3000);
+// console.log("Server started on port 3000, ctrl^c to quit.");
